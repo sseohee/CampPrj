23c7c91e243a9eb52758d734a12360d9dc751f6f
sseohee <tjgml7013@gmail.com>
2021-02-01 19:30:18 +0900

    add with front and back

--- src/components/home/Map.vue
+++ src/components/home/Map.vue
@@ -3,32 +3,24 @@
     <v-container>
       <h1>Map is here</h1>
 
-      <v-app-bar-nav-icon right @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        right
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
 
-      <v-navigation-drawer v-model="drawer" absolute bottom temporary right>
+      <v-navigation-drawer v-model="drawer" absolute bottom temporary right width="25%">
         <v-list nav dense>
           <v-list-item-group
             v-model="group"
             active-class="deep-purple--text text--accent-4"
           >
-           <v-subheader>Camping Search</v-subheader>
-             <v-row align="center">
-     
-       <v-select
-          :items="regions"
-          label="Solo field"
-          dense
-          solo
-        ></v-select>
-        
-        <v-select
-          :items="cities"
-          label="Solo field"
-          dense
-          solo
-        ></v-select>
-     
-    </v-row>
+            <v-subheader>Camping Search</v-subheader>
+            <v-row align="center">
+              <v-select :items="regions" label="Regions" dense solo width="80%"></v-select>
+
+              <v-select :items="cities" label="Cities" dense solo width="80%"></v-select>
+              <v-btn outlined id="Search">Search</v-btn>
+            </v-row>
           </v-list-item-group>
         </v-list>
       </v-navigation-drawer>
@@ -41,15 +33,13 @@ export default {
   name: "map",
   data: () => ({
     drawer: false,
-    group: null, 
-    regions: ['Seoul', 'Incheon', 'Daejeon', 'Gwangju'],
-    cities:['1', '2', '3', '4']
+    group: null,
+    regions: ["Seoul", "Incheon", "Daejeon", "Gwangju"],
+    cities: ["1", "2", "3", "4"],
   }),
 
   watch: {
-    group() {
-      
-    },
+    group() {},
   },
 };
 </script>
